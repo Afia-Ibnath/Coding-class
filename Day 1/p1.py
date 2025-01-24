61c9cb5aee8e908c63d322f8a24f07d9d006c735
Afia-Ibnath <afiaibnathmoynamoti56@gmail.com>
2025-01-24 21:56:10 +0600

    day1

---
+++ Day 1/p1.py
@@ -0,0 +1,12 @@
+print("Hello i am Ai bot, What is your name :")
+name= input()
+print(f"nice to meeting you !{name}")
+print("How are you today ? (good/bad):")
+mood=input().lower()
+if mood=="good":
+    print("i am glad to hear that")
+elif mood =="bad":
+    print("I am sorry to hear that !")
+else:
+    print("I see, some times it may feel same")
+print(f"Its nice to meeting you {name}")
